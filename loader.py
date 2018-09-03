17aa2a8ccc5b3760fe935f4cd92e37785cb3f793
Nicholas Bien <nicholasbien@gmail.com>
2018-09-03 02:46:28 -0700

    remove data augmentation

--- loader.py
+++ loader.py
@@ -36,51 +36,8 @@ class Dataset(data.Dataset):
 
         self.labels = [label_dict[path[6:]] for path in self.paths]
 
-        if self.train:
-            self.horizontal_flip = horizontal_flip
-            self.rotate = rotate
-            self.shift = shift
-
         neg_weight = np.mean(self.labels)
         self.weights = [neg_weight, 1 - neg_weight]
-
-    # shift randomly shifts image slices up to shift_size pixels in rows/cols
-    def _shift(self, arr, shift_size, axis, fill_val=0):
-        result = np.empty_like(arr)
-        shift = np.random.randint(-shift_size, shift_size + 1)
-        fill_slc = [slice(None)] * len(arr.shape)
-        arr_slc = [slice(None)] * len(arr.shape)
-        result_slc = [slice(None)] * len(arr.shape)
-        if shift > 0:
-            fill_slc[axis] = slice(shift)
-            arr_slc[axis] = slice(-shift)
-            result_slc[axis] = slice(shift, None, None)
-            result[tuple(fill_slc)] = fill_val
-            result[tuple(result_slc)] = arr[tuple(arr_slc)]
-        elif shift < 0:
-            fill_slc[axis] = slice(shift, None, None)
-            result_slc[axis] = slice(shift)
-            arr_slc[axis] = slice(-shift, None, None)
-            result[tuple(fill_slc)] = fill_val
-            result[tuple(result_slc)] = arr[tuple(arr_slc)]
-        else:
-            result = arr
-        return result
-            
-    def transform(self, vol):
-        '''
-        Expects vol to have shape (num_slices, depth, rows, cols)
-        '''
-        if self.horizontal_flip:
-            if np.random.rand(1) > .5:
-                vol = np.flip(vol, axis=3).copy()
-        if self.shift != 0:
-            vol = self._shift(vol, self.shift, 2)
-            vol = self._shift(vol, self.shift, 3)
-        if self.rotate != 0:
-            angle = np.random.randint(-self.rotate, self.rotate)
-            vol = rotate(vol, angle, axes=(2,3), reshape=False)
-        return vol
 
     def weighted_loss(self, prediction, target):
         weights_npy = np.array([self.weights[int(t[0])] for t in target.data])
@@ -108,8 +65,8 @@ class Dataset(data.Dataset):
         # convert to RGB
         vol = np.stack((vol,)*3, axis=1)
 
-        if self.train:
-            vol = self.transform(vol)
+        #if self.train:
+        #    vol = self.transform(vol)
 
         vol_tensor = torch.FloatTensor(vol)
         label_tensor = torch.FloatTensor([self.labels[index]])
